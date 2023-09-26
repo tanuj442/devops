7f46b4d7e21253ff6361ad13bbc2f9c251f414e1
tanuj <tanujtammalala2003@gmail.com>
2023-09-26 14:45:23 +0530

    default values in java

---
+++ devops/src/module-info.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module devops {
+	static boolean bool;
+    static byte b;
+    static char c;
+    static short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    
+    public static void main(String args[]) {
+        System.out.println("Default value of boolean: " + bool+" \n Default value of byte: " + b+" \n Default value of char: " + c+"\n Default value of short: " + s+"\n Default value of int: " + i+"\n Default value of long: " + l+"\n Default value of float: " + f+"\n Default value of double: " + d);
+        
+    }
+}
